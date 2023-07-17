c03adf21a8fb25c1a199df7eb116fcb7663bd908
Omar Faruqe <faruqe.cse@gmail.com>
2023-07-17 12:20:44 +0600

    2 problem solve

---
+++ src/Greetings.java
@@ -0,0 +1,14 @@
+public class Greetings {
+    public void sayHello(){
+        System.out.println("Hello");
+    }
+    public void sayBonjour(){
+        System.out.println("Bonjour");
+    }
+    public void saySagotom(){
+        System.out.println("স্বাগতম");
+    }
+    public void sayNiHaow(){
+        System.out.println("你好");
+    }
+}
